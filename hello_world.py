6c80ee7fc305ff45b982662e36663d9c73f95d94
Alex Tarasov <tarasik201617@gmail.com>
2022-06-10 18:32:15 +0200

    Fixing the readable flag.

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ import os
 
 from tqdm import tqdm
 
-with open(os.environ["SOURCE_FILE_TXT"], "wt") as input_file_descriptor:
+with open(os.environ["SOURCE_FILE_TXT"], "rt") as input_file_descriptor:
     with open(os.environ["OUTPUT_FILE_TXT"], "wt") as output_file_descriptor:
         for line in tqdm(input_file_descriptor.readlines()):
             output_file_descriptor.write(f"{line}\n")
