29d630393ce9ddcbd738a6a6d247e3818ef2d6af
Alex Tarasov <tarasik201617@gmail.com>
2022-06-10 18:31:06 +0200

    Changing the source of the test file.

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,8 @@
 import os
 
-import numpy as np
-import scipy as sp
-import pandas as pd
 from tqdm import tqdm
 
-
-with open(os.environ["OUTPUT_FILE_TXT"], "wt") as file_descriptor:
-    for letter in tqdm(list("Hello world")):
-        file_descriptor.write(f"{letter}\n")
+with open(os.environ["SOURCE_FILE_TXT"], "wt") as input_file_descriptor:
+    with open(os.environ["OUTPUT_FILE_TXT"], "wt") as output_file_descriptor:
+        for line in tqdm(input_file_descriptor.readlines()):
+            output_file_descriptor.write(f"{line}\n")
